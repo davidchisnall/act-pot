80a1164050f8903a9a9815ac48285b307ac4279e
Mark Johnston <markjdb@gmail.com>
2022-04-17 12:54:20 -0400

    Update the github-act-runner executable name
    Note that the pot flavours must be reinstalled using "install.sh" to
    apply this change.

--- flavours/github-act.sh
+++ flavours/github-act.sh
@@ -10,7 +10,7 @@ pkg ins -y git-lite node go bash
 git clone https://github.com/ChristopherHX/github-act-runner.git --recursive
 cd github-act-runner/
 go build
-cp main /usr/local/bin/github-act-runner
+cp github-act-runner /usr/local/bin/github-act-runner
 
 # Create the config directory
 mkdir /root/runner
